deea6dcd23f2f23e43a5a25579590d07396adef3
pooja-vish <vishwak@uwindsor.ca>
2026-06-09 01:07:45 -0400

    Pre Order Traversal

---
+++ PreOrderTraversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        findPreorderTraversal(root,result);
+        return result;
+    }
+    public void findPreorderTraversal(TreeNode root, List<Integer> result){
+        if(root == null)
+            return;
+        result.add(root.val);    
+        findPreorderTraversal(root.left,result);
+        findPreorderTraversal(root.right,result);    
+    }
+}
